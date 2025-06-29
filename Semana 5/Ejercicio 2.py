41406b22a2a8960290f9ff2df8fe4e3c65b2c5e6
JORDANVERA005 <159165448+JORDANVERA005@users.noreply.github.com>
2025-06-29 15:46:26 -0500

    .

---
+++ Semana 5/Ejercicio 2.py
@@ -0,0 +1,6 @@
+# Lista de asignaturas
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+
+# Mostrar mensaje para cada asignatura
+for asignatura in asignaturas:
+    print(f"Yo estudio {asignatura}")
